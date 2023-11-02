f747e685c0ecb738396a5b907cee8add4ea1513b
Abdul Safi <hasibsaf90@gmail.com>
2023-11-01 23:59:48 -0700

    initial push

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
+import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
+import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg";
+
+function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true;
+    }
+  };
+  return (
+    <div>
+      <footer className="navbar">
+        <nav className="navbarNav">
+          <ul className="navbarListItems">
+            <li className="navbarListItem">
+              <ExploreIcon
+                fill={pathMatchRoute("/") ? "#2c2c2c" : "#8f8f8f"}
+                width="36px"
+                height="36px"
+              />
+              <p className={"navbarListItemName"}>Swipe</p>
+            </li>
+            <li className="navbarListItem">
+              <OfferIcon
+                fill={pathMatchRoute("/swipe") ? "#2c2c2c" : "#8f8f8f"}
+                width="36px"
+                height="36px"
+              />
+              <p className={"navbarListItemName"}>Liked</p>
+            </li>
+            <li className="navbarListItem">
+              <PersonOutlineIcon
+                fill={pathMatchRoute("/profile") ? "#2c2c2c" : "#8f8f8f"}
+                width="36px"
+                height="36px"
+              />
+              <p className="navbarListItemName">Profile</p>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  );
+}
+
+export default Navbar;

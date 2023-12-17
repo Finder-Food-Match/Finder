2640b9cdeef7145d2ce0f18076d4bd3b94454512
Luis Cedeno <59905362+Lcedeno14@users.noreply.github.com>
2023-12-16 21:21:43 -0800

    liked set and duplicates found

--- server/index.js
+++ server/index.js
@@ -14,8 +14,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
-
-// Set up a Socket.IO connection handler
+const likedRestaurants = new Set();
 io.on("connection", (socket) => {
   console.log(`A user connected ${socket.id}`);
 
@@ -23,16 +22,18 @@ io.on("connection", (socket) => {
     console.log("A user disconnected");
   });
 
-  socket.on("send_message", (data) => {
-    socket.broadcast.emit("receive_message", data);
+  socket.on("restaurant_clicked", (restaurant) => {
+    console.log("Restaurant clicked:", restaurant.name);
+    if (likedRestaurants.has(restaurant.name)) {
+      console.log("Duplicate found");
+      likedRestaurants.clear();
+    } else {
+      likedRestaurants.add(restaurant.name);
+    }
+    socket.broadcast.emit("restaurant_clicked", restaurant);
   });
 
-  // Example: listening for a message event from the client
-  socket.on("message", (message) => {
-    console.log(message);
-    // You can also emit messages to the client
-    io.emit("message", message);
-  });
+  // Other existing socket event handlers...
 });
 
 const PORT = process.env.PORT || 3001;
@@ -40,21 +41,3 @@ server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
 
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-// const { Server } = require("socket.io");
-// const cors = require("cors");
-
-// app.use(cors);
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// server.listen(3001, () => {
-//   console.log("server is running");
-// });
--- src/pages/SampleLobby.jsx
+++ src/pages/SampleLobby.jsx
@@ -1,68 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import io from "socket.io-client";
-import ResturantList from "../assets/Restaurants";
+import RestaurantCard from "../components/RestaurantCard"; // Adjust this import based on your project structure
 
 const socket = io("http://localhost:3001");
 
-function SampleLobby({ RestaurantCard, handleSomething }) {
-  //calling from RL
+function SampleLobby({ ResturantCard,handleSomething }) {
   const handleRestaurantClick = (likedRestaurant) => {
-    console.log("Restaurant clicked:", likedRestaurant);
-    // Further processing...
+    socket.emit("restaurant_clicked", likedRestaurant);
   };
 
-  return (
-    <div>
-      <RestaurantCard handleRestaurantClick={handleRestaurantClick} />
-      {/* other components */}
-    </div>
-  );
-  //react state and hooks
-  const [message, setMessage] = useState("");
-  const [chat, setChat] = useState([]);
-  //emitting messages from socket.io
-  const sendMessage = () => {
-    socket.emit("send_message", message);
-    setMessage(message);
-  };
-  //console.log
-  console.log("these are chats", chat);
-  //message content and setStates
-  const onChange = (e) => {
-    let sampleChat = e.target.value;
-    setMessage(sampleChat);
-  };
   useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setChat((prev) => [...prev, data]);
+    socket.on("restaurant_update", (restaurant) => {
+      console.log("Restaurant clicked:", restaurant);
     });
 
     return () => {
-      socket.off("receive_message");
+      socket.off("restaurant_update");
     };
   }, []);
 
   return (
-    <div>
-      <RestaurantCard handleSomething={handleSomething} />
-    </div>
+      <div>
+        <RestaurantCard handleRestaurantClick={handleRestaurantClick} />
+        {/* other components */}
+      </div>
   );
 }
 
 export default SampleLobby;
-
-{
-  /* <div>
-      <h1>Simple Chat</h1>
-      <input type="text" value={message} onChange={onChange} />
-      <button onClick={sendMessage}>Send</button>
-      </div> */
-}
-
-{
-  /* {chat.map((msg, index) => (
-          <div className="messageCard">
-            <p key={index}>{msg}</p>
-          </div>
-        ))} */
-}
